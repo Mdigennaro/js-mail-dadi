a390cf542ad881bfbae9702925d55f92cd05fc21
Mdigennaro <michele.digennaro719@gmail.com>
2021-10-26 16:01:58 +0200

    inserimento ciclo mail

--- js/script.js
+++ js/script.js
@@ -6,3 +6,52 @@
 3. Stampare la presenza o la non presenza
 */
 
+const mail = [
+  'andreab@gmail.com',
+  'federico@gmail.com',
+  'cristian@gmail.com',
+  'davide@gmail.com',
+  'alberto@gmail.com',
+  'mauro@gmail.com',
+  'matteod@gmail.com',
+  'michele@gmail.com',
+  'matteof@gmail.com',
+  'andreaf@gmail.com',
+  'simone@gmail.com',
+  'mattiag@gmail.com',
+  'larisa@gmail.com',
+  'manuel@gmail.com',
+  'matteol@gmail.com',
+  'francesco@gmail.com',
+  'morena@gmail.com',
+  'mattiam@gmail.com',
+  'simone@gmail.com',
+  'mariano@gmail.com',
+  'vitali@gmail.com',
+  'gianluca@gmail.com',
+  'giuseppe@gmail.com',
+  'salvatore@gmail.com',
+  'giada@gmail.com',
+  'giuliasa@gmail.com',
+  'benjamins@gmail.com',
+  'giuliaso@gmail.com',
+  'lucas@gmail.com',
+  'davide@gmail.com',
+  'luigi@gmail.com',
+  'lucav@gmail.com'
+];
+
+const mailInserita = prompt('Inserisci la tua mail');
+
+
+for (let i = 0; i < mail.length; i++) {
+  const mailPresente = mail[i];
+  
+  if (mailInserita !== mailPresente) {
+    console.log('Mail non presente');
+  } 
+  else {
+    console.log('Mail presente');
+  }
+
+}
